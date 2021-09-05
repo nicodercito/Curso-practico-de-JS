58d3294db3100801a75ccf2fbaa6c31ffe764ee7
Nicolas Quinnonez <naqf94@gmail.com>
2021-09-04 23:01:23 -0500

    Nuevos archivos de descuento

---
+++ descuentos.js
@@ -0,0 +1,33 @@
+let originalPrice = 120;
+const discount = 18;
+
+const priceDiscounted = 100 - discount;
+const priceAdjusted = (originalPrice * priceDiscounted) / 100;
+
+// console.log({
+//     originalPrice,
+//     discount,
+//     priceDiscounted,
+//     priceAdjusted
+// });
+
+function applyPriceDiscounted (originalPrice, discount){
+    const priceDiscounted = 100 - discount;
+    const priceAdjusted = (originalPrice * priceDiscounted) / 100;
+
+    return priceAdjusted;
+}
+
+function onClickButtonPriceDiscount (){
+    const inputPrice = document.getElementById("inputPrice")
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("inputDiscount")
+    const discountValue = inputDiscount.value;
+
+    const resultPrice = document.getElementById("resultPrice");
+
+    const finalPrice = applyPriceDiscounted (priceValue, discountValue)
+    
+    resultPrice.innerText = "El precio con descuento es: $" + finalPrice;
+}
