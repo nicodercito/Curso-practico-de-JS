f65e844fe07aaaf717637141e48abfc3e38e0b37
Nicolas Quinnonez <naqf94@gmail.com>
2021-09-29 16:44:32 -0500

    cambios a html con nuevo formulario

--- descuentos.js
+++ descuentos.js
@@ -33,3 +33,34 @@ function onClickButtonPriceDiscount (){
 
     resultPrice.innerText = "El precio final es: $" + finalPrice;
 }
+
+//Descuentos con objetos
+
+const oxford = {
+    name: 'Oxford',
+    price: 1400,
+    picture: './Oxford.jpg'
+};
+
+const pradera = {
+    name: 'Pradera',
+    price: 1890,
+    picture: './Pradera.jpg'
+};
+
+const queen = {
+    name: 'Queen',
+    price: 2300,
+    picture: './Queen.jpg'
+};
+
+const dresses = [oxford, pradera, queen];
+const discounts = ['Primeriza10', 'MeCasoHoy', 'SoyTuAmiga', 'MeDebesUna'];
+
+function dressDiscount () {
+
+}
+
+function thanks(){
+    prompt ('¡Gracias por tu compra!');
+}
